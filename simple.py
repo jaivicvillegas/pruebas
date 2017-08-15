b7e73334ea0399700a693a61ef6bb2ca275ba350
Jaivic Villegas <jvillegas@caleidos.pe>
2017-08-15 13:11:16 -0500

    se re escribe el archivo de prueba ahora se intenta probar en google

--- simple.py
+++ simple.py
@@ -1,31 +1,25 @@
-import pytest
-
-
-@pytest.mark.usefixtures('driver')
-class TestGuineaPig(object):
-
-    def test_link(self, driver):
-        """
-        Verify page title change when link clicked
-        :return: None
-        """
-        driver.get('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
-        driver.find_element_by_id("i_am_a_link").click()
-
-        title = "I am another page title - Sauce Labs"
-        assert title == driver.title
-
-    def test_comment(self, driver):
-        """
-        Verify comment submission
-        :return: None
-        """
-        driver.get('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
-        sample_text = "hede@hodo.com"
-        email_text_field = driver.find_element_by_id("comments")
-        email_text_field.send_keys(sample_text)
-
-        driver.find_element_by_id("submit").click()
-
-        text = driver.find_element_by_id("your_comments").text
-        assert sample_text in text  
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+ 
+# This is the only code you need to edit in your existing scripts.
+# The command_executor tells the test to run on Sauce, while the desired_capabilities
+# parameter tells us which browsers and OS to spin up.
+desired_cap = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': "31",
+}
+driver = webdriver.Remote(
+   command_executor='http://claudiamarquez:03e669cf-8fe4-46b7-a1ac-59a63b96420a@ondemand.saucelabs.com:80/wd/hub',
+   desired_capabilities=desired_cap)
+ 
+# This is your test logic. You can add multiple tests here.
++driver.implicitly_wait(10)
+ +driver.get("http://www.google.com")
+ +if not "Google" in driver.title:
+ +    raise Exception("Unable to load google page!")
+ +elem = driver.find_element_by_name("q")
+ +elem.send_keys("Sauce Labs")
+ +elem.submit()
+driver.quit()
