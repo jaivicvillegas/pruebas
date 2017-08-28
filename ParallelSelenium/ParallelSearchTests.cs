ac11d6a26b78ede39ae2feb3347aab5ff8ec83cd
Jaivic Villegas <jvillegas@caleidos.pe>
2017-08-28 10:01:09 -0500

    creacion del ruta de conexion en duro

--- ParallelSelenium/ParallelSearchTests.cs
+++ ParallelSelenium/ParallelSearchTests.cs
@@ -39,6 +39,7 @@ namespace ParallelSelenium
             ParallelSearchTests ini = new ParallelSearchTests("chrome", "38", "Windows 7","","");
             ini.Init();
             ini.BingSearchHello();
+            ini.Cleanup();
         }
         [SetUp]
         public void Init()
@@ -74,6 +75,7 @@ namespace ParallelSelenium
             } else {
                 seleniumUri = "https://ondemand.saucelabs.com:443/wd/hub";
             }
+            seleniumUri = "http://claudiamarquez:03e669cf-8fe4-46b7-a1ac-59a63b96420a@ondemand.saucelabs.com:80/wd/hub";
             capabilities.SetCapability("username", Constants.sauceUser);
             capabilities.SetCapability("accessKey", Constants.sauceKey);
             capabilities.SetCapability("name",
