a67d2022c5cc2aa5c682bfce9581f9afd98f8489
Jaivic Villegas <jvillegas@caleidos.pe>
2017-08-28 11:22:06 -0500

    prueba con nueva configuracion y sin main 2

--- ParallelSelenium/ParallelSearchTests.cs
+++ ParallelSelenium/ParallelSearchTests.cs
@@ -34,6 +34,7 @@ namespace ParallelSelenium
             this.os = os;
             this.deviceName = deviceName;
             this.deviceOrientation = deviceOrientation;
+          //  443
         }
         //static void Main() {
         //    ParallelSearchTests ini = new ParallelSearchTests("chrome", "38", "Windows 7","","");
@@ -75,7 +76,7 @@ namespace ParallelSelenium
             } else {
                 seleniumUri = "https://ondemand.saucelabs.com:443/wd/hub";
             }
-           // seleniumUri = "http://claudiamarquez:03e669cf-8fe4-46b7-a1ac-59a63b96420a@ondemand.saucelabs.com:80/wd/hub";
+           // seleniumUri = "http://claudiamarquez:03e669cf-8fe4-46b7-a1ac-59a63b96420a@ondemand.saucelabs.com:443/wd/hub";
             capabilities.SetCapability("username", Constants.sauceUser);
             capabilities.SetCapability("accessKey", Constants.sauceKey);
             capabilities.SetCapability("name",
